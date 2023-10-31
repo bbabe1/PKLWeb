1f759bd0bebd481435af0f58a3087061dd2e7ca7
Ayudia <149310941+bbabe1@users.noreply.github.com>
2023-10-31 03:59:27 -0700

    Add files via upload

---
+++ inventory (1).sql
@@ -0,0 +1,248 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 31, 2023 at 11:58 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `inventory`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fakultas`
+--
+
+CREATE TABLE `fakultas` (
+  `id` int(10) NOT NULL,
+  `nama_fakultas` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notes`
+--
+
+CREATE TABLE `notes` (
+  `id` int(11) NOT NULL,
+  `contents` text NOT NULL,
+  `admin` varchar(20) NOT NULL,
+  `status` varchar(8) NOT NULL DEFAULT 'aktif'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sbrg_keluar`
+--
+
+CREATE TABLE `sbrg_keluar` (
+  `id` int(11) NOT NULL,
+  `idx` int(11) NOT NULL,
+  `tgl` date NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `penerima` varchar(35) NOT NULL,
+  `keterangan` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `sbrg_keluar`
+--
+
+INSERT INTO `sbrg_keluar` (`id`, `idx`, `tgl`, `jumlah`, `penerima`, `keterangan`) VALUES
+(15, 244, '2020-08-29', 1000, 'Kasmina', 'Laku'),
+(19, 247, '2023-09-30', 30, 'Farmasi', '-'),
+(21, 246, '2023-10-17', 8, 'Farmasi', '-'),
+(20, 250, '2023-10-17', 12, 'FKIP', '-'),
+(22, 247, '2023-10-17', 10, 'FKIP', '-'),
+(23, 253, '2024-03-20', 10, 'Farmasi', '-');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sbrg_masuk`
+--
+
+CREATE TABLE `sbrg_masuk` (
+  `id` int(11) NOT NULL,
+  `idx` int(11) NOT NULL,
+  `tgl` date NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `keterangan` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `sbrg_masuk`
+--
+
+INSERT INTO `sbrg_masuk` (`id`, `idx`, `tgl`, `jumlah`, `keterangan`) VALUES
+(9, 244, '2020-08-07', 600, '-'),
+(15, 250, '2023-07-05', 25, '-'),
+(14, 247, '2023-10-09', 10, '-'),
+(16, 248, '2023-10-11', 10, '-');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `slogin`
+--
+
+CREATE TABLE `slogin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nickname` varchar(20) NOT NULL,
+  `role` varchar(10) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `slogin`
+--
+
+INSERT INTO `slogin` (`id`, `username`, `password`, `nickname`, `role`) VALUES
+(7, 'guest', '084e0343a0486ff05530df6c705c8bb4', 'Stock', 'stock');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ssatuan`
+--
+
+CREATE TABLE `ssatuan` (
+  `id` int(6) NOT NULL,
+  `satuan` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sstock_brg`
+--
+
+CREATE TABLE `sstock_brg` (
+  `idx` int(11) NOT NULL,
+  `nama` varchar(55) NOT NULL,
+  `jenis` varchar(30) NOT NULL,
+  `merk` varchar(40) NOT NULL,
+  `ukuran` varchar(20) NOT NULL,
+  `stock` int(12) NOT NULL,
+  `satuan` varchar(10) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `sstock_brg`
+--
+
+INSERT INTO `sstock_brg` (`idx`, `nama`, `jenis`, `merk`, `ukuran`, `stock`, `satuan`) VALUES
+(243, 'Mata Bor', 'Flame', 'Garryson', '50', 2992, 'Buah'),
+(250, 'Sticky Note', 'Sticky Note warna', 'Joyko', 'Kecil 1 cm', 43, 'Pack'),
+(246, 'Boxfile', 'Boxfile biasa', 'Bantex', '45 x 36', 12, 'Buah'),
+(247, 'Map kertas', 'Stopmap folio', 'Stopmap Biola 5002', 'Kertas A4', 70, 'Lembar'),
+(248, 'Cutter', 'Cutter Barang', 'Cutter Vanco V-500', 'Besar', 35, 'Buah'),
+(253, 'BUKU ', 'BUKU TULIS', 'SIDU', 'Kecil', 0, 'Buah');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `fakultas`
+--
+ALTER TABLE `fakultas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `notes`
+--
+ALTER TABLE `notes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sbrg_keluar`
+--
+ALTER TABLE `sbrg_keluar`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sbrg_masuk`
+--
+ALTER TABLE `sbrg_masuk`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `slogin`
+--
+ALTER TABLE `slogin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ssatuan`
+--
+ALTER TABLE `ssatuan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sstock_brg`
+--
+ALTER TABLE `sstock_brg`
+  ADD PRIMARY KEY (`idx`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `notes`
+--
+ALTER TABLE `notes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `sbrg_keluar`
+--
+ALTER TABLE `sbrg_keluar`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `sbrg_masuk`
+--
+ALTER TABLE `sbrg_masuk`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `slogin`
+--
+ALTER TABLE `slogin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `ssatuan`
+--
+ALTER TABLE `ssatuan`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sstock_brg`
+--
+ALTER TABLE `sstock_brg`
+  MODIFY `idx` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=254;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
